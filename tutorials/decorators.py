7ee762f4717b662630fe1e4077b5cd3292e2ba25
Ishak <ishak.ariif@gmail.com>
2024-12-05 16:52:56 +0000

    Further fixes

--- tutorials/decorators.py
+++ tutorials/decorators.py
@@ -7,11 +7,11 @@ def user_type_required(allowed_types):
         @wraps(view_func)
         def wrapped(request, *args, **kwargs):
             if not request.user.is_authenticated:
-                return redirect('login')
+                return redirect('log_in')
             if request.user.type not in allowed_types:
                 return render(request, '403.html', {
-                    'message': "You don't have permission to access this page.",
-                    'redirect_url': 'tutor_dashboard' if request.user.type == 'tutor' else 'dashboard'
+                    'message': "You don't have permission to access this page.", 
+                    'redirect_url': 'student_dashboard' if request.user.type == 'student' else 'tutor_dashboard'
                 }, status=403)
             return view_func(request, *args, **kwargs)
         return wrapped
