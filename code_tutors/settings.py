f2f9c9a0b04c095cc1ca1106a67c3ed9655b6240
DavidYouyouWu <k21185211@kcl.ac.uk>
2024-12-12 18:16:03 +0000

    Fix bugs

--- code_tutors/settings.py
+++ code_tutors/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&$dln5wpgorppuw&(gintxm573v2ks+zq4o$(4*lapguixf^+2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['youyouwu.pythonanywhere.com', '127.0.0.1']
 
